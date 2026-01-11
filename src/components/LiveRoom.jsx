f8a3abfddf385bea437b979db4cfab005a652d58
kori5181 <stevekoruthu@gmail.com>
2026-01-11 21:30:33 +0530

    DIBS LOGO height up and offline/live tag above also (UI changes)

--- src/components/LiveRoom.jsx
+++ src/components/LiveRoom.jsx
@@ -342,8 +342,8 @@ export const LiveRoom = ({ roomId }) => {
         
         <div className="flex justify-between items-start pointer-events-auto">
             <div className="flex flex-col items-start gap-0.5">
-                <img src="/Dibs. (1).svg" alt="Dibs" className="w-28 -ml-4"/>
-                <div className={`px-2 py-0.5 rounded-sm flex items-center gap-2 ${isStreaming ? 'bg-red-600' : 'bg-neutral-800'}`}>
+                <img src="/Dibs. (1).svg" alt="Dibs" className="w-28 -ml-4 -mt-7 block"/>
+                <div className={`-mt-5 px-2 py-0.5 rounded-sm flex items-center gap-2 ${isStreaming ? 'bg-red-600' : 'bg-neutral-800'}`}>
                     <span className="font-display font-black text-xs camelcase tracking-widest text-white">
                         {isStreaming ? 'LIVE' : 'OFFLINE'}
                     </span>
